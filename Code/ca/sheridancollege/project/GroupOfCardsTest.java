a3563fa4cf085b9ea5d50cdf005911342400e52c
Vallecampo <eduardavi@protonmail.com>
2022-04-19 22:51:19 -0400

    change test class

---
+++ Code/ca/sheridancollege/project/GroupOfCardsTest.java
@@ -0,0 +1,38 @@
+import org.junit.Test;
+
+public class GroupOfCardsTest {
+    @Test
+    public void testGetCard() {
+
+    }
+
+    @Test
+    public void testGetCards() {
+
+    }
+
+    @Test
+    public void testGetSize() {
+
+    }
+
+    @Test
+    public void testRemoveCard() {
+
+    }
+
+    @Test
+    public void testSetCards() {
+
+    }
+
+    @Test
+    public void testShowCards() {
+
+    }
+
+    @Test
+    public void testShuffle() {
+
+    }
+}
